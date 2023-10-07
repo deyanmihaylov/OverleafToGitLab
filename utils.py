8d0ad431cd7ba18bb046bf4613769777c4c6f2db
Deyan Mihaylov <d.p.mihaylov@gmail.com>
2023-10-06 23:15:32 -0400

    Add file utils.py

---
+++ utils.py
@@ -0,0 +1,7 @@
+def get_Overleaf_url_from_hash(hash_slug: str) -> str:
+    url = f"https://git.overleaf.com/{hash_slug}"
+    return url
+
+def get_hash_from_Overleaf_url(url: str) -> str:
+    hash_slug = url.rsplit('/', 1)[-1]
+    return hash_slug
